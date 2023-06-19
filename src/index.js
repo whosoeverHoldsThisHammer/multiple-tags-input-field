b41567526b60c5a3ce0bce358ee1f0107d515943
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2023-06-19 12:06:17 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import m from "mithril";
-m.render(document.body, "hello world");
+
+const root = document.getElementById("app")
+
+const App = {
+    view: function (vnode) {
+        return m("div", { class: "container mx-auto"} , [
+            m("input", { "class": "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6", "type": "text", "name": "search", "id": "search", "placeholder": "Search..." })
+        ])
+    }
+}
+
+m.mount(root, App);
